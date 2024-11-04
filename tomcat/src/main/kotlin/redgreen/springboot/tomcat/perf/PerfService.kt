d9f46e653a48dcee4184f229310e1f8e7ba079fa
Alphonse Bendt <alphonse.bendt@gmail.com>
2024-11-04 21:37:09 +0100

    cleanup

--- tomcat/src/main/kotlin/redgreen/springboot/tomcat/perf/PerfService.kt
+++ tomcat/src/main/kotlin/redgreen/springboot/tomcat/perf/PerfService.kt
@@ -1,19 +1,14 @@
 package redgreen.springboot.tomcat.perf
 
-import io.opentelemetry.instrumentation.annotations.WithSpan
-import kotlin.time.measureTimedValue
 import org.springframework.stereotype.Service
 import sample.cpuIntensiveTask
 
 @Service
 class PerfService {
-
-    @WithSpan("performCpuWork")
     fun performCpuWork(): String {
         return cpuIntensiveTask(1000, 1000).toString()
     }
 
-    @WithSpan("blockingIO")
     fun performIoWork() {
         Thread.sleep(500)
     }
